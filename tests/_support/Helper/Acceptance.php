1bf8d63846eb25210236dae2cf548f6f40cfc879
Cameron Eagans <me@cweagans.net>
2016-12-19 18:13:24 -0700

    Clean up composer.lock and vendor dirs after tests run

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -28,6 +28,12 @@ class Acceptance extends \Codeception\Module
     {
         $filesystem = new Filesystem();
         $filesystem->remove($this->_getPluginDir());
+
+        $composerLockFiles = glob($this->_getFixturesDir() . '/*/composer.lock');
+        $filesystem->remove($composerLockFiles);
+
+        $composerVendorDirs = glob($this->_getFixturesDir() . '/*/vendor');
+        $filesystem->remove($composerVendorDirs);
     }
 
     protected function _getFixturesDir()
